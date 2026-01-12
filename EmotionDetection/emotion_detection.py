93026c784317bf3aa1db2c6734941e8216e11cd6
Abhishek Shrestha <19162467+shresthabhishek@users.noreply.github.com>
2026-01-12 00:42:15 -0500

    Fix pylint issues

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,26 +2,38 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    # handle blank entries
+    if not text_to_analyze:
+        return {'dominant_emotion': None}
+    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
+    myobj = {"raw_document": { "text": text_to_analyze }}
     response = requests.post(url, json = myobj, headers=headers)
 
     formatted_response = json.loads(response.text)
 
     # find the dominant emotion
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    if response.status_code == 200:
+        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
 
-    dominant_emotion_score = -1
-    dominant_emotion = None
-    for emotion, score in emotion_scores.items():
-        
-        if score > dominant_emotion_score:
-            dominant_emotion = emotion
-            dominant_emotion_score = score
+        dominant_emotion_score = -1
+        dominant_emotion = None
+        for emotion, score in emotion_scores.items():
             
-    
-    #return response.text;
-    emotion_scores['dominant_emotion'] = dominant_emotion 
+            if score > dominant_emotion_score:
+                dominant_emotion = emotion
+                dominant_emotion_score = score
+                
+        
+        #return response.text;
+        emotion_scores['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        #  return the same dictionary, but with values for all keys being None
+        for emotion, score in emotion_scores.items():
+            emotion_scores[emotion] = None
+
+        emotion_scores['dominant_emotion'] = None
 
     return emotion_scores
+    
--- server.py
+++ server.py
@@ -1,3 +1,11 @@
+'''
+Module for detecting emotions of the user provided input using AI
+
+Executing this function initiates the application of emotion detection
+to be executed over the Flask channel and deployed on
+localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
@@ -6,13 +14,19 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the emotion score as well as the dominant emotion
+        for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     emotion_scores = emotion_detector(text_to_analyze)
-    
-    #print(emotion_scores)
+
+    if emotion_scores['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
 
     resp_text = "For the given statement, the system response is"
-    
+
     cnt = len(emotion_scores)
     i = 0
     for emo, score in emotion_scores.items():
@@ -22,15 +36,18 @@ def emo_detector():
         elif i == cnt -1:
             # remove the trailing comma
             resp_text = resp_text[:-1]
-            resp_text += " and '{}': {}".format(emo, score)
+            resp_text += f" and '{emo}': {score}"
         else:
-            resp_text += " '{}': {},".format(emo, score)
+            resp_text += f" '{emo}': {score},"
 
     return resp_text
 
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
